dfaa2e4e01c65f3b913794bfa7999c9177a85135
jwang9228 <wang.justi@northeastern.edu>
2023-11-25 00:50:35 -0500

    Resolve issue with API_BASE

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_BASE = process.env.REACT_APP_API_BASE_A6;
+const API_BASE = process.env.REACT_APP_API_BASE;
 const MODULES_URL = `${API_BASE}/modules`;
 const COURSES_URL = `${API_BASE}/courses`;
 
